027c3f97e942cd366a6ddc4abb29364b4e3708f1
Arjun Bhadandari <arjun12345bhandari@gmail.com>
2025-07-13 11:19:17 +0530

    Initial Setup Frontend

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import ausadhiSewa from  './assets/AusadhiSewa.logo.png'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={ausadhiSewa} className="logo" alt="Ausadhi-Sewa logo" />
+        </a>
+       
+      </div>
+      <h1>Ausadhi Sewa</h1>
+    
+    </>
+  )
+}
+
+export default App
